ba2ab44e0571d39eec18d4f1181fc7be94451f10
Chris Carlson <cwcarlson10@gmail.com>
2015-02-03 22:47:44 -0700

    added some more stying to differentiate between charges and finished tests

--- Charges/spec/requests/credit_card_charges_spec.rb
+++ Charges/spec/requests/credit_card_charges_spec.rb
@@ -6,16 +6,20 @@ describe "cretdit_card_charges" do
     it "should have the h1 'Successful Charges'" do
       visit '/credit_card_charges/index'
       page.all('h1.Successful Charges')
+      page.all('div.well',    count: 10)
     end
 
     it "should have the h1 'Disputed Charges'" do
       visit '/credit_card_charges/index'
       page.all('h1.Disputed Charges')
+      page.all('div.disputed-charges')
+
     end
 
     it "should have the h1 'Failed Charges'" do
       visit '/credit_card_charges/index'
       page.all('h1.Failed Charges')
+      page.all('div.failed-charges',    count: 5)
     end
   end
 end
